4825183b7dc6da1701b87659925b945a1d9558a4
Jonathan <69145546+jonathan-d-zhang@users.noreply.github.com>
2022-10-13 23:54:14 -0400

    Add ability to run tests, capture stdout
    At this point, unable to see output of panic
    
    Add mold for `config.toml` and coverage workflow. Achieved 4x speedup in
    linking time locally.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,48 +1,134 @@
+/// Integration test
+///
+/// This implements an extremely scuffed custom test harness in order to have setup and teardown
+/// functionality. It tries to emulate the output of libtest, but can't really handle anything
+/// other than `cargo test`.
 #[macro_use]
 extern crate lazy_static;
 
 use graphql_client::{reqwest::post_graphql_blocking as post_graphql, GraphQLQuery};
 use reqwest::blocking::Client;
-use std::sync::Once;
 use std::thread;
+use std::panic;
+use std::collections::HashMap;
+use std::process::ExitCode;
+use std::sync::Mutex;
 
 const URL: &str = "http://localhost:8080/graphql";
 const TEST_QUOTE: &str = "The quick brown fox jumps over the lazy dog. Can't-I'm<>12932. Cwm fjord bank glyphs vext quiz!";
-static SETUP: Once = Once::new();
 
-lazy_static! {
+lazy_static!{
     static ref CLIENT: Client = Client::new();
+    static ref STDOUT: Mutex<HashMap<&'static str, Vec<u8>>> = Mutex::new(HashMap::new());
+}
+
+fn main() -> ExitCode {
+    setup();
+
+    let tests: &[(fn(), &str)] = &[
+        (test_api_version, "test_api_version"),
+        (test_cryptogram_identity_medium, "test_cryptogram_identity_medium"),
+        (test_cryptogram_encrypt_identity, "test_cryptogram_encrypt_identity"),
+    ];
+
+    println!("running {} tests", tests.len());
+
+    let mut errors = Vec::with_capacity(tests.len());
+    for (test, name) in tests.into_iter() {
+        let res = run_test(*test);
+        let output = match res {
+            Ok(_) => "\x1b[32mok\x1b[37m",
+            Err(e) => {
+                errors.push((name, e));
+                "\x1b[31mFAILED\x1b[37m"
+            }
+        };
+        println!("test {name} ... {output}");
+    }
+
+    let _ = panic::take_hook();
+
+    if !errors.is_empty() {
+        println!("\nfailures:");
+        for (&name, error) in &errors {
+            let spacer = "-".repeat(4);
+            println!("{spacer} {name} stdout {spacer}");
+            print!("{}", String::from_utf8(STDOUT.lock().unwrap()[name].clone()).unwrap());
+
+            if let Some(p) = error.downcast_ref::<String>() {
+                println!("{p:?}");
+            } else if let Some(p) = error.downcast_ref::<&str>() {
+                println!("{p:?}");
+            }
+        }
+
+        println!("\nfailures:");
+        for (name, _) in &errors {
+            println!("\t{name}")
+        }
+    }
+
+    println!("\ntest result {} failed; {} passed", errors.len(), tests.len() - errors.len());
+
+    if errors.is_empty() {
+        ExitCode::SUCCESS
+    } else {
+        ExitCode::FAILURE
+    }
+}
+
+struct Stdout {
+    name: &'static str,
+    buf: Vec<u8>,
+}
+
+impl Stdout {
+    pub fn new(name: &'static str) -> Self {
+        Self {
+            name,
+            buf: Vec::new(),
+        }
+    }
+}
+
+impl Drop for Stdout {
+    fn drop(&mut self) {
+        STDOUT.lock().unwrap().insert(self.name, self.buf.clone());
+    }
+}
+
+fn run_test(test: fn()) -> Result<(), Box<dyn std::any::Any + Send>> {
+    panic::catch_unwind(test)
 }
 
 fn setup() {
-    SETUP.call_once(|| {
-            // setup tempfile for quotes
-            println!("Temp dir {:?}", std::env::temp_dir());
-            let tf = tempfile::Builder::new()
-                .prefix("test-quotes")
-                .suffix(".json")
-                .append(true)
-                .tempfile_in(std::env::temp_dir())
-                .unwrap();
+    // setup tempfile for quotes
+    let tf = tempfile::Builder::new()
+        .prefix("test-quotes")
+        .suffix(".json")
+        .append(true)
+        .tempfile_in(std::env::temp_dir())
+        .unwrap();
 
-            let path = tf.path();
-            println!("Wrote to {:?}", path);
+    let path = tf.path();
 
-            std::fs::write(&path,
-                r#"[{"quote": "The quick brown fox jumps over the lazy dog. Can't-I'm<>12932. Cwm fjord bank glyphs vext quiz!", "author": "jz9", "genre": "testing"}]"#
-                .as_bytes(),
-            )
-            .unwrap();
+    std::fs::write(&path,
+        r#"[{"quote": "The quick brown fox jumps over the lazy dog. Can't-I'm<>12932. Cwm fjord bank glyphs vext quiz!", "author": "jz9", "genre": "testing"}]"#
+        .as_bytes(),
+    )
+    .unwrap();
 
-            std::env::set_var(
-                "QUOTES_FILE",
-                path,
-            );
-        thread::spawn(|| {
-            cryptograms::make_server();
-        });
-        thread::sleep(std::time::Duration::from_secs(3));
+    std::env::set_var(
+        "QUOTES_FILE",
+        path,
+    );
+    thread::spawn(|| {
+        cryptograms::make_server();
     });
+    thread::sleep(std::time::Duration::from_secs(3));
+
+    // set a hook that doesn't print as soon as a panic occurs
+    panic::set_hook(Box::new(|_| {}));
 }
 
 #[derive(GraphQLQuery)]
@@ -61,22 +147,20 @@ pub struct Version;
 )]
 pub struct Cryptogram;
 
-#[test]
 fn test_api_version() {
-    setup();
+    let mut stdout = Stdout::new("test_api_version");
 
     let response_body = post_graphql::<Version, _>(&CLIENT, URL, version::Variables).unwrap();
 
     let data: version::ResponseData = response_body.data.unwrap();
-    println!("{:?}", data);
+
+    stdout.buf.extend_from_slice(format!("{:?}", data).as_bytes());
 
     assert_eq!(data.api_version, "0.1")
 }
 
-#[test]
 fn test_cryptogram_identity_medium() {
-    setup();
-
+    let mut stdout = Stdout::new("test_cryptogram_identity_medium");
     let variables = cryptogram::Variables {
         plaintext: None,
         type_: Some(cryptogram::Type::IDENTITY),
@@ -86,7 +170,8 @@ fn test_cryptogram_identity_medium() {
     let response_body = post_graphql::<Cryptogram, _>(&CLIENT, URL, variables).unwrap();
 
     let data: cryptogram::ResponseData = response_body.data.unwrap();
-    println!("{:?}", data);
+
+    stdout.buf.extend_from_slice(format!("{:?}", data).as_bytes());
 
     assert_eq!(
         data.cryptogram.ciphertext,
@@ -94,10 +179,8 @@ fn test_cryptogram_identity_medium() {
     )
 }
 
-#[test]
 fn test_cryptogram_encrypt_identity() {
-    setup();
-
+    let mut stdout = Stdout::new("test_cryptogram_encrypt_identity");
     let variables = cryptogram::Variables {
         plaintext: Some(TEST_QUOTE.to_string()),
         type_: None,
@@ -107,7 +190,7 @@ fn test_cryptogram_encrypt_identity() {
     let response_body = post_graphql::<Cryptogram, _>(&CLIENT, URL, variables).unwrap();
 
     let data: cryptogram::ResponseData = response_body.data.unwrap();
-    println!("{:?}", data);
+    stdout.buf.extend_from_slice(format!("{:?}", data).as_bytes());
 
     assert_eq!(data.cryptogram.ciphertext, TEST_QUOTE.to_string(),)
 }
